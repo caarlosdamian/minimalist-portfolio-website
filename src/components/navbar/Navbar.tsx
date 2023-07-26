52bcd797535077cf962afd14f1de47c5f2ba5481
caarlosdamian <dapecarlos@gmail.com>
2023-07-26 17:05:47 -0600

    feat(menu): added menu component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,10 +2,12 @@ import { NavLink } from 'react-router-dom';
 import { hambuerger, logo } from '../../assets';
 import { navItems } from '../../utils';
 import styles from './Navbar.module.scss';
+import { Menu } from '../menu/Menu';
 
 export const Navbar = () => {
   return (
     <nav className={styles.container}>
+      <Menu />
       <div className={styles.left}>
         <img src={logo} alt="logo" />
       </div>
