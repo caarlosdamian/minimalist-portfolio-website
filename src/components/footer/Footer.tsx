02ad71f3d38efd69c32c8825b44bef4885d48e38
caarlosdamian <dapecarlos@gmail.com>
2023-07-29 20:25:13 -0600

    feat(components): added footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,11 +5,13 @@ import { logoLight } from '../../assets';
 import { socialIcons } from '../../utils/info';
 
 export const Footer = () => {
+  
   const links = navItems.map(({ id, label, path }) => (
     <NavLink to={path} key={id}>
       {label}
     </NavLink>
   ));
+
   const socials = socialIcons.map(({ id, icon }) => (
     <img src={icon} alt={id} key={id} />
   ));
